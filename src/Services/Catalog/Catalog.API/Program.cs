55928fbd85d0c9cbf01d435cc02ac8c45b4eafb4
rashed.tareq@gmail.com <rashed.tareq@gmail.com>
2025-05-30 11:18:22 +0600

    Added Global Exception To The Middleware

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,6 +2,8 @@
 #region Register Services To The Container
 
 using BuildingBlocks.Behaviors;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,6 +35,34 @@ builder.Services.AddMarten(opts =>
 var app = builder.Build();
 
 app.MapCarter();
+
+//Global Exception Middleware
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception is null)
+        {
+            return;
+        }
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+
+        var logger = context.RequestServices.GetService<ILogger<Program>>();
+        logger.LogError(exception, exception.Message, problemDetails);
+
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+
+        await context.Response.WriteAsJsonAsync(problemDetails);
+    });
+});
 
 app.Run();
 
