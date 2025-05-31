7b38562c521445c13f6779a0698247a3f030be12
rashed.tareq@gmail.com <rashed.tareq@gmail.com>
2025-06-01 00:19:40 +0600

    Added Sqlite Database With Migrations

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,14 +1,23 @@
+using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Add Grpc Service
 builder.Services.AddGrpc();
+
+// Add Sqlite Database Service
+builder.Services.AddDbContext<DiscountContext>(op =>
+    op.UseSqlite(builder.Configuration.GetConnectionString("Database")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapGrpcService<DiscountService>();
+
+app.MapGet("/", () => "");
 
 app.Run();
