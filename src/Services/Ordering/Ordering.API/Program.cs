3280a4e3e4873a349546e940c1d95deef40b811c
rashed.tareq@gmail.com <rashed.tareq@gmail.com>
2025-06-02 23:52:30 +0600

    Added Project References and DependencyInjection For Service Registration

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,7 +1,15 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Serviceses To The Container
 
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
 
 var app = builder.Build();
 
