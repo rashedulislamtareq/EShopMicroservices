3259da641c18e172dbaf87dfd8c714049f03f75d
rashed.tareq@gmail.com <rashed.tareq@gmail.com>
2025-06-24 23:55:40 +0600

    Registered EFCore DbContext For Dependency Injection

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Data;
 
 namespace Ordering.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration) 
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        var connectionString = configuration.GetConnectionString("Database");
 
-        return services; 
+        services.AddDbContext<ApplicationDbContext>(op => op.UseSqlServer(connectionString));
+
+        return services;
     }
 }
