a6ac34d3d28e275e4438364e22a5bcdea3a98fdb
Rashedul Islam <rashedul.islam@technonext.com>
2026-09-14 18:58:14 +0600

    Added Swagger And Tested Created Endpoint

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,27 +1,29 @@
 ﻿namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
-public record CreateProductResponse(Guid Id);
 
+public record CreateProductResponse(Guid Id);
 
 public class CreateProductEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
-        {
-            var command = request.Adapt<CreateProductCommand>();
+        app.MapPost("/products",
+            async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
 
-            var result = await sender.Send(command);
+                var result = await sender.Send(command);
 
-            var response = result.Adapt<CreateProductResponse>();
+                var response = result.Adapt<CreateProductResponse>();
 
-            return Results.Created($"/products/{response.Id}", response);
-        })
-            .WithName("CreateProduct")
-            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
-            .ProducesProblem(StatusCodes.Status400BadRequest)
-            .WithSummary("Create Product")
-            .WithDescription("Create Product");
+                return Results.Created($"/products/{response.Id}", response);
+
+            })
+        .WithName("CreateProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Product")
+        .WithDescription("Create Product");
     }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,16 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //Add Services To The Container
+var assembly = typeof(Program).Assembly;
 
 builder.Services.AddCarter();
 
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.RegisterServicesFromAssembly(assembly);
 });
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
 //Configure The Pipeline
+
+// Enable Swagger middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapCarter();
 
 app.Run();
