8c3f8e86f6d2a0be32fa399a6970707802ecde9e
Rashedul Islam <rashedul.islam@technonext.com>
2026-09-17 13:05:28 +0600

    Added CRUD Endpoints For Products

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+public record GetProductByCategoryResult(IEnumerable<Product> products);
+
+
+public class GetProductByCategoryHandler(IDocumentSession sesson)
+    : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+{
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        var products = await sesson.Query<Product>()
+                .Where(p => p.Category.Contains(query.Category))
+                .ToListAsync(cancellationToken);
+
+        return new GetProductByCategoryResult(products);
+    }
+}
