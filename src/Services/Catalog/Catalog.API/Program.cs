1bec29141164208f38888463c1b7054f20154a3a
Rashedul Islam <rashedul.islam@technonext.com>
2026-09-20 12:05:03 +0600

    Run Catalog API In The Docker Environment

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,14 @@
+using Catalog.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add Services To The Container
 var assembly = typeof(Program).Assembly;
 
+// For Carter Endpoint
 builder.Services.AddCarter();
 
+// For MediatR & Pipeline Behavior
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(assembly);
@@ -12,21 +16,29 @@ builder.Services.AddMediatR(config =>
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
 
+//For Marten Database Framework
 builder.Services.AddMarten(op =>
 {
     op.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
 
+//For Fluent Validation
 builder.Services.AddValidatorsFromAssembly(assembly);
 
 // Add Swagger services
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//For Health Cheak UI
 builder.Services.AddHealthChecks()
     .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
 
+//For Global Exception Handler
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+if (builder.Environment.IsDevelopment())
+    // For Marten Seed data
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
 
 var app = builder.Build();
 
@@ -35,14 +47,19 @@ var app = builder.Build();
 // Enable Swagger middleware
 if (app.Environment.IsDevelopment())
 {
+
+    // For Swagger UI
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// For carter Endpoint
 app.MapCarter();
 
+//For Global Exception Handler
 app.UseExceptionHandler(op => { });
 
+//For Health Cheak UI
 app.UseHealthChecks("/health",
     new HealthCheckOptions()
     {
